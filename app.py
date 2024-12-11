ebd50d8eec76647cb17821e39af172f3245dd15b
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2024-12-11 15:39:41 +0700

    fix error

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from routes.diabetes_routes import diabetes_bp
 from controllers.diabetes_controller import load_model
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(diabetes_bp, url_prefix='/api')
@@ -9,4 +14,9 @@ app.register_blueprint(diabetes_bp, url_prefix='/api')
 load_model()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Gunakan variabel environment untuk konfigurasi
+    debug_mode = os.getenv("DEBUG", "False").lower() == "true"
+    port = int(os.getenv("PORT", 5000))
+    
+    # Jalankan aplikasi pada host 0.0.0.0 agar dapat diakses dari luar
+    app.run(debug=debug_mode, host="0.0.0.0", port=port)
